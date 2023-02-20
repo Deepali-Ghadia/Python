11148339908a9929b2b5ae8a83ab0b958f8a4a5e
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-20 21:47:14 +0530

    learned basic datatypes in python

--- Chapter-2/data_types.py
+++ Chapter-2/data_types.py
@@ -1,10 +1,10 @@
-# string datatype
+# datatype: string
 str1 = "APPLE"
 print(str1[0])
 print(str1[1])
 print(str1[:4])
 
-# set datatype
+# datatype: set
 fruits = {'apple', 'banana', 'orange', 'mango', 'apple', 'orange', 'orange'}
 print(fruits) # duplicates will be removed
 print("New item added")
@@ -15,3 +15,24 @@ print(fruits)
 print("Learning about frozen sets")
 vegetables = frozenset({'tomato', 'potato', 'cabbage', 'tomato', 'cauliflower', 'potato'})
 print(vegetables)
+
+# datatype: number
+sum = 100;
+print("Sum is: ", sum)
+division = 1/3;
+print("Division is: ", division)
+
+# datatype: list
+fruits = ['apple', 'banana', 'orange', 'mango', 'apple', 'orange', 'orange']
+print(fruits[0])
+print(fruits[5])
+print(fruits[-1])
+
+# datatype: dictionary
+dict = {'color': 'red', 'name': 'abc'}
+print(dict)
+print(dict.keys())
+
+# datatype: tuples
+tup = ('apple', 'banana', 'orange', 'mango', 'apple', 'orange', 'orange')
+print(tup)
