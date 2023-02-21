8bd80c2acf3eb96611ab46832498efe5059e6c59
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-21 22:46:52 +0530

    implemented division, addition and exponentiation

---
+++ Chapter-9/operators.py
@@ -0,0 +1,41 @@
+# simple mathematical operators
+import math
+a = 10
+b = 20
+c = 23
+d = 2
+l1 = [1,2,3]
+l2 = [4,5,6]
+t1 = (1,2,3)
+t2 = (3,4,5)
+# division
+print(a/b)
+print(a/c)
+print(a/d)
+# print(a//b)gives integer value by taking floor value
+print(a//c)
+print(a//d)
+
+
+# addition
+print(a+b)
+print(l1 + l2)
+print(t1 + t2)
+
+
+# Exponentiation
+a1 = 2
+a2 = 3
+print(a1**a2)
+print(math.pow(2,3)) # gives answers in float
+# Trigonometric Functions
+
+# Inplace operations
+
+# Subtraction
+
+# Multiplication
+
+# Logarithms
+
+# Modulus
