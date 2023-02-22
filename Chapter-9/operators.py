375bf49019d41abad8d2dc348b36096fb7ce35ef
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-22 17:36:20 +0530

    learned operators in python

--- Chapter-9/operators.py
+++ Chapter-9/operators.py
@@ -1,5 +1,7 @@
 # simple mathematical operators
 import math
+import operator
+
 a = 10
 b = 20
 c = 23
@@ -8,6 +10,7 @@ l1 = [1,2,3]
 l2 = [4,5,6]
 t1 = (1,2,3)
 t2 = (3,4,5)
+
 # division
 print(a/b)
 print(a/c)
@@ -28,14 +31,20 @@ a1 = 2
 a2 = 3
 print(a1**a2)
 print(math.pow(2,3)) # gives answers in float
+print(operator.pow(2,3)); # gives answer in integer value
+
+
 # Trigonometric Functions
+print(math.sin(90)) # returns sin(90) in radians
+print(math.hypot(3,4)) # calculates the hypotenuse
 
 # Inplace operations
+a += 1
+print(a)
+a -= 1
+print(a)
 
-# Subtraction
-
-# Multiplication
-
-# Logarithms
 
 # Modulus
+print(10%5)
+print(operator.mod(10,5))
