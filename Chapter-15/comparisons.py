cded65cf2b6961e8be9800a08e50a58afd99e2b8
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-22 21:55:24 +0530

    learned the difference between == and is comparisons

--- Chapter-15/comparisons.py
+++ Chapter-15/comparisons.py
@@ -1,21 +1,17 @@
 # chain comparison
 a = 23
-# b = 15
-# c = 89
+b = 15
+c = 89
 d = 23
-# print(a > b < c == d)
+print(a > b < c == d)
 
 
 # comparison by is vs "=="
 # == compares the values
 # compares the identities i.e ids of both the elements
-# print(a==d)
-# print(id(a))
-# print(id(d))
-# print(a is d) # bcoz both variables are referring to same value
+print(a==d)
+print(id(a))
+print(id(d))
+print(a is d) # bcoz both variables are referring to same value
 
-# But longer strings and larger integers will be stored separately
-a = 'this is longgg textttttttttttttttttttttttttttttttttttttttttttt'
-b = 'this is longgg textttttttttttttttttttttttttttttttttttttttttttt'
-print(a==b)
-print(a is b) 
+
