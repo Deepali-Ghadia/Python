e732b6125ff937a3b992c309f26708cec921985f
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-22 21:17:49 +0530

    implemented conditional statements in python

---
+++ Chapter-14/conditional_statements.py
@@ -0,0 +1,21 @@
+# ternary operator
+num = int(input())
+print("Even Number") if num%2==0 else print("Odd Number")
+
+# if/ else-if/ else statement
+if num%2 == 0:
+    print("Number is Even")
+elif num%2 != 0:
+    print("Number is Odd")
+else:
+    print("Enter correct input")
+    
+Boolean logic expression
+output is either true or false
+a = 5
+b = 7
+print(a or b)
+print(a and b)
+print(1 and "Hello, World")
+print(1 or "Hello, World")
+
