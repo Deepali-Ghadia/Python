0f4521df561d1a36728291693b17f44dbf861a81
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-23 17:48:10 +0530

    implemented multi-dimensional array in python

---
+++ Chapter-18/multi_dimensional_arrays.py
@@ -0,0 +1,19 @@
+# list in list i.e. 2D 
+list1 = [[1,2,3], 
+         [4,5,6], 
+         [7,8,9]]
+
+print(list1[0])
+print(list1[0][0])
+print(list1[0][1])
+print(list1[0][2])
+
+print(list1[1])
+print(list1[1][0])
+print(list1[1][1])
+print(list1[1][2])
+
+print(list1[2])
+print(list1[2][0])
+print(list1[2][1])
+print(list1[2][2])
