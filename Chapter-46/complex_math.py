88a8bbf4cbc955db59d43276ae7766f02aba9a86
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-25 17:47:27 +0530

    learned about cmath module

---
+++ Chapter-46/complex_math.py
@@ -0,0 +1,15 @@
+# complex maths using cmath
+import cmath
+complex_number = 2 + 3j
+# calculating phase using phase function
+print(cmath.phase(complex_number))
+print(cmath.sin(complex_number)) # prints sin value of a complex number
+
+
+z1 = 3 + 2j
+z2 = 2 + 1j
+
+print(z1 + z2)
+print(z1 - z2)
+print(z1 * z2)
+print(z1 / z2)
