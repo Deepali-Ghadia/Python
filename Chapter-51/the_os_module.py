294837d82186e06f7ad16c04c26a8e7005030e06
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-26 20:42:23 +0530

    implemented various functions provided by os module

---
+++ Chapter-51/the_os_module.py
@@ -0,0 +1,12 @@
+import os
+
+# create directory recursively using OS Module => create directory inside a directory that does not exist. All the directories will be created using the command ==>
+# os.makedirs('Chapter-51/dir1/subdir1/sub-subdir1')
+
+# create a directory
+# os.mkdir('Chapter-51/dir2')
+
+# get current directory
+# print(os.getcwd())
+
+print(os.name)
