eb329a83131de8ad059e835f0c96b0f7198d8edf
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-27 22:23:03 +0530

    created virtual environment in python

---
+++ Chapter-174/python_virtual_env.py
@@ -0,0 +1,8 @@
+# installing virtual environment
+    # pip install virtualenv
+# creating a virtual environment
+    # virtualenv <name_of_virtualenv>
+# activate the virtual environment to use it
+    #  source <name_of_virtualenv>/bin/activate
+# deactivate virtualenv
+    # deactivate
