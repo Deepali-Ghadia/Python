711bbc4ac64798e9208e650271d2939bf39ab238
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-27 12:08:54 +0530

    implemented exponentiation function

---
+++ Chapter-70/exponentiation.py
@@ -0,0 +1,14 @@
+import math
+# exponentiation using pow function
+print(math.pow(12, 2))
+
+
+# modular exponentiation
+# Supplying pow() with 3 arguments pow(a, b, c) evaluates the modular exponentiation ab mod c:
+print(pow(3, 4, 17))
+
+
+# exponential function minus 1
+#  expm1()-function that can compute the expression math.e ** x - 1 for very small x
+print(math.expm1(1e-3))
+
