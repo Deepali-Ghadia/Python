29954a9a371addf709e88166bfc8b33c49c4ce84
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-27 21:15:26 +0530

    learned to create virtual environment in python

---
+++ Chapter-173/virtual_environments.py
@@ -0,0 +1,14 @@
+# A Virtual Environment is a tool to keep the dependencies required by different projects in separate places, by creating virtual Python environments for them.
+
+
+# creating and using a virtual environment
+''' 
+step1: installing a virtual environment tool => pip install virtualenv
+step2: creating a new virtual environment => virtualenv <name_of_env>
+    accessing an existing virtual environment => <name_of_env/Scripts/ativate.bat
+'''
+
+
+# creating a virtual environment with specific version of python
+# virtualenv -p python3 <name_of_env>   OR
+# virtualenv --python=python3 <name_of_env>
