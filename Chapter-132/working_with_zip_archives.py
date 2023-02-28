6cee5b121dfd318238fdd8655bce4ad43698baf2
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-28 16:00:23 +0530

    learned to perform more operations on zipfile

--- Chapter-132/working_with_zip_archives.py
+++ Chapter-132/working_with_zip_archives.py
@@ -7,3 +7,6 @@ with ZipFile("C:/Users/user/OneDrive/Desktop/Mobile_Wallpaper.zip") as zip:
 # extracting zipfile contents to a directory
 with ZipFile("C:/Users/user/OneDrive/Desktop/Mobile_Wallpaper.zip") as zip:
     zip.extractall('Chapter-132/')
+    
+    
+    
