0f073d96c1232652d171afc5fe954b9159bb091b
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-02-28 10:20:14 +0530

    learned to create a simple HTTP server in python

---
+++ Chapter-124/python_networking.py
@@ -0,0 +1,9 @@
+# creating a simple HTTP server
+# python -m http.server  => type in terminal
+# by default selects port number 8000
+# You can access to your files through any device connected to the local network by typing http://hostipaddress:8000/. ====> http://192.168.1.203:8000
+
+# press Ctrl+C to close the module
+
+
+# creating a TCP server using the sockerserver library
